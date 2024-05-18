daa9b97baf646567b975519f8a0f83b491a10979
JoshiCodes <55353244+JoshiCodes@users.noreply.github.com>
2024-05-18 13:15:01 +0200

    created the AGHPBook class

--- src/main/java/de/joshicodes/aghpb4j/objects/AGHPBook.java
+++ src/main/java/de/joshicodes/aghpb4j/objects/AGHPBook.java
@@ -1,11 +1,10 @@
 package de.joshicodes.aghpb4j.objects;
 
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
 
 public record AGHPBook(
         byte[] imageBytes,
+        BookImageType type,
         String category,
         String commitAuthor,
         String commitHash,
@@ -15,13 +14,19 @@ public record AGHPBook(
         int searchId
 ) {
 
+    public enum BookImageType {
+        PNG, JPEG
+    }
+
     public void saveImage(File file) throws IOException {
         if(file == null) throw new IllegalArgumentException("File cannot be null. Please provide a valid file.");
-        if(!file.exists()) file.getParentFile().mkdirs();
+        if(!file.exists()) {
+            if(file.getParentFile() != null) file.getParentFile().mkdirs();
+        }
         if(!file.getName().endsWith(".png") && !file.getName().endsWith(".jpg") && !file.getName().endsWith(".jpeg")) throw new IllegalArgumentException("File must be a valid image file (png, jpg, jpeg).");
-        FileWriter writer = new FileWriter(file);
-        writer.write(new String(imageBytes));
-        writer.close();
+        try (OutputStream os = new FileOutputStream(file)) {
+            os.write(imageBytes);
+        }
     }
 
 }
