a24a2bdcedc52d2e96cd6c3a2636207a149a83aa
JoshiCodes <55353244+JoshiCodes@users.noreply.github.com>
2024-06-16 12:10:29 +0200

    Updated to Version v1.2
    Created AGHPB#retrieveSearch and AGHPB#retrieveBook

--- src/main/java/de/joshicodes/aghpb4j/AGHPB.java
+++ src/main/java/de/joshicodes/aghpb4j/AGHPB.java
@@ -152,6 +152,147 @@ public class AGHPB {
     }
 
     /**
+     * Retrieves a List of AGHPBooks from the AGHPB API. <br>
+     * <b>NOTE:</b> The AGHPBooks will not contain any image data. Use #retrieveBook(String) with the search_id.<br>
+     * @param query The query to search for.
+     * @return A RestAction for the search.
+     */
+    public RestAction<List<AGHPBook>> retrieveSearch(String query) {
+        return retrieveSearch(query, null, -1);
+    }
+
+    /**
+     * Retrieves a List of AGHPBooks from the AGHPB API. <br>
+     * <b>NOTE:</b> The AGHPBooks will not contain any image data. Use #retrieveBook(String) with the search_id.<br>
+     * @param query The query to search for.
+     * @param limit The limit of books to retrieve. Must be greater than 0.
+     * @return A RestAction for the search.
+     */
+    public RestAction<List<AGHPBook>> retrieveSearch(String query, int limit) {
+        return retrieveSearch(query, null, limit);
+    }
+
+    /**
+     * Retrieves a List of AGHPBooks from the AGHPB API. <br>
+     * <b>NOTE:</b> The AGHPBooks will not contain any image data. Use #retrieveBook(String) with the search_id.<br>
+     * @param query The query to search for.
+     * @param category The category to search in. Can be null.
+     * @return A RestAction for the search.
+     */
+    public RestAction<List<AGHPBook>> retrieveSearch(String query, String category) {
+        return retrieveSearch(query, category, -1);
+    }
+
+    /**
+     * Retrieves a List of AGHPBooks from the AGHPB API. <br>
+     * <b>NOTE:</b> The AGHPBooks will not contain any image data. Use #retrieveBook(String) with the search_id.<br>
+     * @param query The query to search for.
+     * @param category The category to search in. Can be null.
+     * @param limit The limit of books to retrieve. Must be greater than 0.
+     * @return A RestAction for the search.
+     */
+    public RestAction<List<AGHPBook>> retrieveSearch(String query, String category, int limit) {
+        final String url = this.url + "/search?query=" + query + (category != null ? "&category=" + category : "") + (limit > 0 ? "&limit=" + limit : "");
+        return new RestAction<>(url, "GET", null, (response) -> {
+            JsonElement json = response.getAsJsonElement();
+            if(json == null || !json.isJsonArray()) {
+                throw new IllegalStateException("Response is not a JSON array");
+            }
+            JsonArray array = json.getAsJsonArray();
+            List<AGHPBook> books = new ArrayList<>();
+            for(JsonElement element : array) {
+                if(!element.isJsonObject()) {
+                    continue;
+                }
+                JsonObject obj = element.getAsJsonObject();
+                if(
+                        !obj.has("search_id")
+                        || !obj.has("name")
+                        || !obj.has("category")
+                        || !obj.has("date_added")
+                        || !obj.has("commit_url")
+                        || !obj.has("commit_author")
+                        || !obj.has("commit_hash")
+                ) {
+                    continue;
+                }
+                final int searchId = Integer.parseInt(obj.get("search_id").getAsString());
+                final String name = obj.get("name").getAsString();
+                final String category1 = obj.get("category").getAsString();
+                final String dateAdded = obj.get("date_added").getAsString();
+                final String commitUrl = obj.get("commit_url").getAsString();
+                final String commitAuthor = obj.get("commit_author").getAsString();
+                final String commitHash = obj.get("commit_hash").getAsString();
+                books.add(new AGHPBook(null, null, category1, commitAuthor, commitHash, commitUrl, name, searchId));
+            }
+            return books;
+        });
+    }
+
+    /**
+     * Retrieves a book from the AGHPB API. <br>
+     * This Method is used, to get the image data of a book, which was retrieved by #retrieveSearch(String) and does not contain any image data. <br>
+     * @param book The book to retrieve.
+     * @return An ImageAction for the book.
+     */
+    public ImageAction<AGHPBook> retrieveBook(AGHPBook book) {
+        return retrieveBook(book.searchId());
+    }
+
+    /**
+     * Retrieves a book from the AGHPB API. <br>
+     * The returned book will contain the image data in PNG. <br>
+     * @param searchId The search_id of the book to retrieve.
+     * @return An ImageAction for the book.
+     */
+    public ImageAction<AGHPBook> retrieveBook(int searchId) {
+        return retrieveBook(searchId, AGHPBook.BookImageType.PNG);
+    }
+
+    /**
+     * Retrieves a book from the AGHPB API. <br>
+     * @param book The book to retrieve.
+     * @param type The format of the book.
+     * @return An ImageAction for the book.
+     */
+    public ImageAction<AGHPBook> retrieveBook(AGHPBook book, AGHPBook.BookImageType type) {
+        return retrieveBook(book.searchId(), type);
+    }
+
+    /**
+     * Retrieves a book from the AGHPB API. <br>
+     * @param searchId The search_id of the book to retrieve.
+     * @param type The format of the book.
+     * @return An ImageAction for the book.
+     */
+    public ImageAction<AGHPBook> retrieveBook(int searchId, AGHPBook.BookImageType type) {
+        final String url = this.url + "/book?search_id=" + searchId;
+        return new ImageAction<>(url, AGHPBook.class, (request) -> {
+            request.header("Accept", "image/" + type.name().toLowerCase());
+            return request;
+        }, response -> {
+            final String contentType = response.httpResponse().headers().firstValue("Content-Type").orElse(null);
+            if(contentType == null || !contentType.startsWith("image/")) {
+                JsonElement json = response.getAsJsonElement();
+                throw new IllegalStateException("Response is not an image! " + (json != null ? ("( " + json + " )") : ""));
+            }
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+            try {
+                baos.write(response.httpResponse().body());
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+            final String imageCategory = response.httpResponse().headers().firstValue("Book-Category").orElse(null);
+            final String bookName = response.httpResponse().headers().firstValue("Book-Name").orElse(null);
+            final String commitAuthor = response.httpResponse().headers().firstValue("Book-Commit-Author").orElse(null);
+            final String commitHash = response.httpResponse().headers().firstValue("Book-Commit-Hash").orElse(null);
+            final String commitUrl = response.httpResponse().headers().firstValue("Book-Commit-Url").orElse(null);
+            // final Date dateAdded = new Date(response.httpResponse().headers().firstValue("Book-Date-Added").orElse(null));
+            return new AGHPBook(baos.toByteArray(), type, imageCategory, commitAuthor, commitHash, commitUrl, bookName, searchId);
+        });
+    }
+
+    /**
      * Retrieves the info of the AGHPB API. <br>
      * The Info contains the book count and the API version. <br>
      * @return A RestAction for the info.
